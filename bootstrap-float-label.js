80a8fd56e0ddac7b8f3f23a30aa201ac8c240f70
William Garrido <wgarrido@oxibis.com>
2017-06-02 13:37:05 +0200

    rename js file

---
+++ bootstrap-float-label.js
@@ -0,0 +1,22 @@
+jQuery.bootstrapFloatLabel = function () {
+  $("form .float-label label").addClass("hide-label");
+
+  //Verifie si le place holder est vide
+  $("form .float-label input").each(function () {
+    var placeholder = $(this).attr('placeholder');
+    console.log(placeholder)
+    var label = $(this).parent().find("label").text();
+    if (placeholder === undefined || placeholder === "") {
+      $(this).attr("placeholder", label);
+    }
+  });
+
+  //Ajoute ou supprime la class header-label
+  $("form .float-label input").bind("input propertychange", function (e) {
+    if ($(e.target).val() !== "") {
+      $(e.target).parent().find("label").addClass("header-label");
+    } else {
+      $(e.target).parent().find("label").removeClass("header-label");
+    }
+  });
+}
